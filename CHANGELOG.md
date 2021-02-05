3021bbd833f3ff2ce588b6c6918c6c0248aed55b
andres <velasco.andrs@gmail.com>
2021-02-05 13:40:26 -0500

    update license and changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,4 @@
-## [0.0.1] - TODO: Add release date.
-
-* TODO: Describe initial release.
+## [0.0.1] - 2021-02-05.
+Init package 
+## [0.0.2] - 2021-02-05. 
+Update LICENSE
--- lib/custom_route_transitions.dart
+++ lib/custom_route_transitions.dart
@@ -1,4 +1,4 @@
-library route_transitions;
+library custom_route_transitions;
 
 import 'package:flutter/material.dart';
 
@@ -9,14 +9,14 @@ enum AnimationType {
   fadeIn,
 }
 
-class RouteTransitions {
+class CustomRouteTransitions {
   final BuildContext context;
   final Widget child;
   final AnimationType animation;
   final Duration duration;
   final bool replacement;
 
-  RouteTransitions({
+  CustomRouteTransitions({
     @required this.context,
     @required this.child,
     this.animation = AnimationType.normal,
